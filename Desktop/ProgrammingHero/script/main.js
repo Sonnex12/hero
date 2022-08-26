82fd9abe9be527d0c9806965edb3433c9c866dcf
sundayOga <sundayogaabraham@gmail.com>
2022-08-26 16:50:24 +0100

    commiting to github a project done to test my skill, celebrate my new skills

---
+++ Desktop/ProgrammingHero/script/main.js
@@ -0,0 +1,15 @@
+function displayHide(){
+    document.getElementById('link').style.display="block";     
+  }
+  
+  function Undo(){
+     document.getElementById('link').style.display="none"; 
+  }
+  function undo(){
+   document.getElementById('sub').innerHTML="Subscribed";
+   
+  }
+
+function ondo(){
+   document.getElementById('sub2').innerHTML="Subscribed";
+}
